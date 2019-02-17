759c674579ae3158a43a65f166ad714dc6b051a6
manasi9307 <manasi.thakur@sjsu.edu>
2019-02-17 13:56:54 -0800

    Paricle Js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 //import logo from './logo.svg';
 import './App.css';
 
+
+const particleOptions = {
+  "particles": {
+    "number": {
+        "value": 100
+    },
+    "size": {
+        "value": 3
+    }
+},
+"interactivity": {
+    "events": {
+        "onhover": {
+            "enable": true,
+            "mode": "repulse"
+        }
+    }
+}
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+                params={particleOptions} />
         <Navigation />
          <Logo />
+         <Rank />
         <ImageLinkForm />
         {/*<Facerecognition /> */}
       </div>
