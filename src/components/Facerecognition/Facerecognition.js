a599ae96c854353f8fe494e42905780bbb01f018
manasi9307 <manasi.thakur@sjsu.edu>
2019-02-17 18:46:11 -0800

    Complete

--- src/components/Facerecognition/Facerecognition.js
+++ src/components/Facerecognition/Facerecognition.js
@@ -6,7 +6,7 @@ const Facerecognition = ({imageUrl,box}) => {
     <div className='center pa2'>
     <div className='absolute mt2'>
     <img id='inputimage' src="imageUrl" alt='output' width='500px' height='auto' />
-    <div className='bounding-box'>
+    <div className='bounding-box' style={{top: box.leftCol,right: box.topRow, bottom: box.rightCol, left: box.botRow}}>
 
     </div>
     </div>
