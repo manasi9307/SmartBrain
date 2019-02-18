926ff21a66078e109cea3b3b0f00f56e67843600
manasi9307 <manasi.thakur@sjsu.edu>
2019-02-17 22:46:14 -0800

    Register and SignIn

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Facerecognition from './components/Facerecognition/Facerecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 //import logo from './logo.svg';
 import './App.css';
 
@@ -17,20 +19,112 @@ const app = new Clarifai.App({
 const particleOptions = {
   "particles": {
     "number": {
-        "value": 100
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
     },
     "size": {
-        "value": 3
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
     }
-},
-"interactivity": {
+  },
+  "interactivity": {
+    "detect_on": "canvas",
     "events": {
-        "onhover": {
-            "enable": true,
-            "mode": "repulse"
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
         }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
     }
-}
+  },
+  "retina_detect": true
 }
 class App extends Component {
   constructor(){
@@ -38,12 +132,13 @@ class App extends Component {
     this.state = {
       input:'',
       imageUrl: '',
-      box:{}
+      box:{},
+      route: 'signin'
     }
   }
 
   calculateFacelocation = (data) => {
-    const clarifiaFace = response.outputs[0].data.regions[0].regioninfo.boundingbox;
+    const clarifiaFace = data.outputs[0].data.regions[0].regioninfo.boundingbox;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
@@ -52,13 +147,14 @@ class App extends Component {
       leftCol:clarifiaFace.left_col * width /100,
       topRow:clarifiaFace.right_col * height/100,
       rightCol:width - (clarifiaFace.right_col * width),
-      botRow: heigth - (clarifiaFace.topRow * height)
+      botRow: height - (clarifiaFace.botRow * height)
     }
   }
 
-  displayFaceBox = (box){
-    this.setState({box: box})
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
+
   onInputChange = (event) => {
   this.setState({ input: event.target.value });
   }
@@ -68,7 +164,7 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     console.log(this.imageUrl);
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      console.log("here");
+      //console.log("here");
     function(response) {
      this.displayFaceBox(this.calculateFacelocation(response)) ;
     },
@@ -77,16 +173,36 @@ class App extends Component {
     }
   );
   }
+
+  onRouteChange = (route) => {
+    this.setState({route});
+  }
+
   render() {
     return (
       <div className="App">
       <Particles className='particles'
                 params={particleOptions} />
-        <Navigation />
-         <Logo />
-         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
-        <Facerecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
+                
+        
+        {this.state.route === 'home' ?
+        <div>
+          <Navigation onRouteChange = {this.onRouteChange} />
+        <Logo />
+       <Rank />
+       
+      <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+      <Facerecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
+      
+      </div>
+         : 
+         (
+           this.state.route === 'signin' ?
+           <SignIn onRouteChange = {this.onRouteChange} />
+           : <Register onRouteChange = {this.onRouteChange} />
+         )
+      
+      }
       </div>
     );
   }
