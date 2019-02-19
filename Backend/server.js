61a791516124454fe663fd5e1855d5f30c0d7f6d
manasi9307 <manasi.thakur@sjsu.edu>
2019-02-18 18:13:38 -0800

    Backend upload

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
